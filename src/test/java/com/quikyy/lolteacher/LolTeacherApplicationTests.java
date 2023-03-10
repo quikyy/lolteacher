8b5cec08d4e3bfac864acc2d7b9dd30815218982
quikyy <paczuskiszymon16@gmail.com>
2023-03-10 12:52:55 +0100

    Initial commit

---
+++ src/test/java/com/quikyy/lolteacher/LolTeacherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quikyy.lolteacher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolTeacherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
