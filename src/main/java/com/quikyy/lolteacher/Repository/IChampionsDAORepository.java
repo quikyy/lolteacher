6fd0f18ff8ba5d9e5643a061e8b77f6b92f05ce1
quikyy <paczuskiszymon16@gmail.com>
2023-03-14 20:52:46 +0100

    repository fix

--- src/main/java/com/quikyy/lolteacher/Repository/IChampionsDAORepository.java
+++ src/main/java/com/quikyy/lolteacher/Repository/IChampionsDAORepository.java
@@ -2,10 +2,14 @@ package com.quikyy.lolteacher.Repository;
 
 import com.quikyy.lolteacher.Model.DAO.ChampionDAO;
 import java.math.BigInteger;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IChampionsDAORepository extends PagingAndSortingRepository<ChampionDAO, BigInteger> {
 
+	@Query(value = "SELECT * FROM champions WHERE slug_name = :championSlug", nativeQuery = true)
+	ChampionDAO getChampionBySlug(@Param("championSlug") String championSlug);
 }
